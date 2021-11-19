3015049ae9bc32ea1208012732046037c2a80dd3
Hammad <hammad2091504@hotmail.com>
2021-11-19 00:21:07 +0000

    Modify CSS style sheet

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,35 +1,70 @@
-function runGame(){
+// variables
+const buttons = document.getElementsByTagName("button");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const choices = ["rock", "paper", "scissors"];
+
+/**
+ * Add event listeners to control panel buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", funtion() {
+        let playerChoice = this.getAttritube("data-type");
+        playGame(playerChoice);
+    });
+}
+
+/**
+ * Main game function. It reads the data-type of the selected button
+ * and generates and random choice from the array
+ * @param {*} playerChoices 
+ */
+function playGame(playerChoice){
+    playerImage.src = `assests/images/player-${choices[playerChoice]}.png`;
+    let comupterChoice = Math.floor(Math.random() * 3);
+    computerImage.src = `assests/images/computer-${choices[playerChoice]}.png`;
+    let result = checkWinner(choices[playerChoice], choices[computerChoice]);
+
+    checkWinner(result);
 
 }
 
-function checkAnswer(){
+
+/**
+ * Compares playerChoice and computerChoice data string to check winner of game
+ */
+function checkWinner(playerChoice, computerChoice) {
+    /* if player choices rock*/
+    if (playerChoice === "rock" && computerChoice === "rock"){
+        document.get
+    }
 
 }
 
-function compareAnswer(){
+function compareAnswer() {
 
 }
 
-function resultAnswer(){
+function resultAnswer() {
 
 }
 
-function incrementScore(){
+function incrementScore() {
 
 }
 
-function incrementlose(){
+function incrementlose() {
 
 }
 
-function battleLost(){
+function battleLost() {
 
 }
 
-function battleWon(){
+function battleWon() {
 
 }
 
-function enemyDefeated(){
-    
+function enemyDefeated() {
+
 }
