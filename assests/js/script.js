93d7edbd00ad0a9d46c6da30624eade36321885c
Hammad <hammad2091504@hotmail.com>
2021-12-01 15:50:03 +0000

    Add Modal pop up for rules button

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,90 +1,43 @@
-// variables
-let rulesBtn= document.getElementById('rules-btn');
-const buttons = document.getElementsByTagName("button");
-const playerImage = document.getElementById("player-image");
-const computerImage = document.getElementById("computer-image");
-const choices = ["rock", "paper", "scissors"];
+/**
+ * Funtion to open and close the rules button
+ */
+// Get the modal
+let modal = document.getElementById("rules-modal");
+
+// Get the button that opens the modal 
+let btn = document.getElementById("rules-button");
+btn.addEventListener('click', modalPopup);
+
+// When the user clicks the button, open the modal 
+function modalPopup() {
+    modal.style.display = "block";
+}
+
+// Get the <span> element that closes the modal and close it 
+let span = document.getElementsByClassName("close")[0];
+span.addEventListener('click', closeModal);
+function closeModal(){
+    modal.style.display="none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 /**
- * Add event listeners to control panel buttons
+ * Add event listener to the player control buttons
  */
-for (let button of buttons) {
-    button.addEventListener("click", funtion() {
-        let playerChoice = this.getAttritube("data-type");
+let buttons=document.getElementsByTagName('button');
+for (let button of buttons){
+    buttons.addEventListener('click', function(){
+        let playerChoice= this.getAttribute ("data-type");
         playGame(playerChoice);
     });
 }
 
-/**
- * Main game function. It reads the data-type of the selected button
- * and generates and random choice from the array
- */
-function playGame(playerChoice){
-    playerImage.src = `assests/images/player-${choices[playerChoice]}.png`;
-    let comupterChoice = Math.floor(Math.random() * 3);
-    computerImage.src = `assests/images/computer-${choices[playerChoice]}.png`;
-    let result = checkWinner(choices[playerChoice], choices[computerChoice]);
-
-    checkWinner(result);
-
-}
 
 
-/**
- * Compares playerChoice and computerChoice data string to check winner of game
- */
-function checkWinner(playerChoice, computerChoice) {
-    /* if player choices rock*/
-    if (playerChoice === "rock" && computerChoice === "rock"){
-        document.getElementById('battle-result').innerHTML="Draw!";
-    }
-    else if (playerChoice === "rock" && computerChoice === "paper"){
-        document.getElementById('battle-result').innerHTML="Battle Lost!";
-    }
-    else if( playerChoice === "rock" && computerChoice === "scissors"){
-        document.getElementById('battle-result').innerHTML="Battle Won!";
-    }
-    else if(playerChoice === "paper" && computerChoice === "rock"){
-        document.getElementById('battle-result').innerHTML="Battle Won!";
-    }
-    else if( playerChoice === "paper" && computerChoice === "paper"){
-        document.getElementById('battle-result').innerHTML="Draw!";
-    }
-    else if (playerChoice === "paper" && computerChoice === "scissors"){
-        document.getElementById('battle-result').innerHTML="Battle Lost!";
-    }
-    else if (playerChoice === "scissors" && computerChoice === "rock"){
-        document.getElementById('battle-result').innerHTML="Battle Lost!";
-    }
-    else if (playerChoice === "scissors" && computerChoice === "paper"){
-        document.getElementById('battle-result').innerHTML="Battle Won!";
-    }
-    else if (playerChoice === "scissors" && computerChoice === "scissors"){
-        document.getElementById('battle-result').innerHTML="Draw!";
-    }
-}
 
-function compareAnswer() {
-
-}
-
-
-function incrementScore() {
-
-}
-
-function incrementlose() {
-
-}
-
-function battleLost() {
-
-}
-
-function battleWon() {
-
-}
-
-function enemyDefeated() {
-
-}
