10cb1cb89ecffed4f3b860a7de2b1dbd13b9cab0
Hammad <hammad2091504@hotmail.com>
2021-12-01 19:48:46 +0000

    Add checkWinner function

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,8 +1,8 @@
 //Variables
-let buttons = document.getElementsByTagName("buttons");
+let buttons = document.getElementsByTagName("button");
 let weapons=["rock","paper","scissors"]
-let playerImage=document.getElementById("player-image");
-let enemyImage=document.getElementById("enemy-image");
+let playerImage = document.getElementById("player-image");
+let enemyImage = document.getElementById("enemy-image");
 
 /**
  * Funtion to open and close the rules button
@@ -49,18 +49,59 @@ for (button of buttons) {
  * and generates a random computer response from the weapons array
  */
 function playGame(playerWeapon){
-  playerImage.src=`assets/images/${weapons[playerWeapon]}-control-button.png`;
-  let enemyWeapon= Math.floor(Math.random() * 3);
-  enemyImage.src=`assets/images/${weapons[enemyWeapon]}-control-button.png`;
+  playerImage.src=`/assets/images/${playerWeapon}-control-button.png`;
+  let enemyWeapon= Math.floor(Math.random() * weapons.length);
+  enemyImage.src=`/assets/images/${enemyWeapon}-control-button.png`;
 
-  let result= checkWinner(weapons[playerWeapon], weapons[enemyWeapon]);
+  let result= checkWinner(playerWeapon, enemyWeapon);
 
   checkWinner(result);
   trackRound();
 
 }
 
-function checkWinner(){
+/**
+ * Compares playerWeapon and enemyWeapon data strings to check winner of game
+ */
+ function checkWinner(playerWeapon, enemyWeapon) {
+  /* If player chooses rock */
+  if (playerWeapon === "rock" && enemyWeapon === "rock") {
+    document.getElementById("battle-result").innerHTML = "Draw!";
+  }
+  else if (playerWeapon === "rock" && enemyWeapon === "paper") {
+    document.getElementById("battle-result").innerHTML = "You Lose! Paper Covers Rock";
+    addEnemyScore();
+  }
+  else if (playerWeapon === "rock" && enemyWeapon === "scissors") {
+    document.getElementById("battle-result").innerHTML = "You Win! Rock Crushes Scissors";
+    addPlayerScore();
+  }
+  
+  /* If chooses paper */
+  else if (playerWeapon === "paper" && enemyWeapon === "paper") {
+    document.getElementById("battle-result").innerHTML = "Draw!";
+  }
+  else if (playerWeapon === "paper" && enemyWeapon === "rock") {
+    document.getElementById("battle-result").innerHTML = "You Win! Paper Covers Rock";
+    addPlayerScore();
+  }
+  else if (playerWeapon === "paper" && enemyWeapon === "scissors") {
+    document.getElementById("battle-result").innerHTML = "You Lose! Scissors Cuts Paper";
+    addEnemyScore();
+  }
+
+  /* If player chooses scissors */
+  else if (playerWeapon === "scissors" && enemyWeapon === "scissors") {
+    document.getElementById("battle-result").innerHTML = "Draw!";
+  }
+  else if (playerWeapon === "scissors" && enemyWeapon === "rock") {
+    document.getElementById("battle-result").innerHTML = "You Lose! Rock Crushes Scissors";
+    addEnemyScore();
+  }
+  else if (playerWeapon === "scissors" && enemyWeapon === "paper") {
+    document.getElementById("battle-result").innerHTML = "You Win! Scissors Cuts Paper";
+    addPlayerScore();
+  }
 
 }
 
